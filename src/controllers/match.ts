ee44c44629c8bb47ee4bc819eabcb1aa551e09b0
Temirzhan Yussupov <timka2609@gmail.com>
2022-08-14 17:55:39 +0600

    feat: update match api

--- src/controllers/match.ts
+++ src/controllers/match.ts
@@ -1,11 +1,12 @@
-import { RequestHandler, Response } from "express";
+import { Request, RequestHandler, Response } from "express";
 
 import { Match } from "../models";
 
 export const matchController: RequestHandler = async (
-  _,
+  request: Request,
   response: Response
 ) => {
-  const matches = await Match.find({ over: false });
+  const over = !!request.params.over;
+  const matches = await (over ? Match.find({ over: false }) : Match.find());
   return response.status(200).json(matches);
 };
